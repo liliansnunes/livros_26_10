a9951fa0bccc18dda3274c5350d07dd50b3dfcb6
liliansnunes <liliansnunes@outlook.com.br>
2021-10-26 17:29:25 +0000

    update

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -2,11 +2,8 @@ package application.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import application.repositories.LivroRepository;
-
 import org.springframework.ui.Model;
-
 @Controller
 @RequestMapping("/livro")
 public class LivroController {
@@ -15,7 +12,7 @@ public class LivroController {
     @RequestMapping("/list")
     public String list(Model model) {
         model.addAttribute("livros", livrosRepo.findAll());
-        return "list.jsp"; 
+        return "list.jsp";
     }
   public String listar(){
     return "livro/list.jsp";
@@ -24,5 +21,8 @@ public class LivroController {
   public String formInsert(){
     return "insert.jsp";
   }
-
+  @RequestMapping("/insert",method=RequestMethod.POST)
+  public String formInsert(){
+    return "insert.jsp";
+  }
 }
